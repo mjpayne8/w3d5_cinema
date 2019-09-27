ccd40542be30683078878053a868a6815ae296a4
Mark Payne <mjpayne8@gmail.com>
2019-09-27 13:26:07 +0100

    delete method added to ticket class

--- models/ticket.rb
+++ models/ticket.rb
@@ -28,6 +28,13 @@ class Ticket
     SqlRunner.run(sql,values)
   end
 
+  def delete()
+    sql = "DELETE FROM tickets
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner(sql, values)
+  end
+
   def self.all()
     sql = "SELECT * from tickets"
     return SqlRunner.run(sql).map {|ticket| Ticket.new(ticket)}
