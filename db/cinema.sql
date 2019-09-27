6c87d7ad528d45739b73d7ad3d295bb74c8e69ca
Mark Payne <mjpayne8@gmail.com>
2019-09-27 12:09:14 +0100

    initial commit

---
+++ db/cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT
+);
+
+CREATE TABLE films (
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT
+);
+
+CREATE TABLE tickets (
+  id SERIAL8 PRIMARY KEY,
+  customer_id INT8 REFERENCES customers(id),
+  film_id INT8 REFERENCES films(id)
+);
