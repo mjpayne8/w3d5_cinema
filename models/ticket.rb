656c1da9960623f0222301b43a2548a43df865ce
Mark Payne <mjpayne8@gmail.com>
2019-09-27 12:45:56 +0100

    Ticket class save method written

--- models/ticket.rb
+++ models/ticket.rb
@@ -11,4 +11,12 @@ def Ticket
     @film_id = options['film_id'].to_i
   end
 
+  def save()
+    sql = 'INSERT INTO tickets
+      (customer_id, film_id)
+      VALUES ($1, $2)'
+    values = [@customer_id, @film_id]
+    SqlRunner.run(sql,values)
+  end
+
 end
