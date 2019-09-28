0668be43f7790f5dc27016763cc7db18363b6646
Mark Payne <mjpayne8@gmail.com>
2019-09-28 14:05:36 +0100

    ticket class changed to reflect changes to tables

--- models/ticket.rb
+++ models/ticket.rb
@@ -3,28 +3,28 @@ require_relative('../db/sql_runner')
 
 class Ticket
 
-  attr_accessor(:customer_id, :film_id)
+  attr_accessor(:customer_id, :screening_id)
   attr_reader(:id)
 
   def initialize( options )
     @id = options['id'].to_i if options['id']
     @customer_id = options['customer_id'].to_i
-    @film_id = options['film_id'].to_i
+    @screening_id = options['screening_id'].to_i
   end
 
   def save()
     sql = "INSERT INTO tickets
-      (customer_id, film_id)
+      (customer_id, screening_id)
       VALUES ($1, $2) RETURNING id"
-    values = [@customer_id, @film_id]
+    values = [@customer_id, @screening_id]
     @id  = SqlRunner.run(sql,values)[0]['id']
   end
 
   def update()
     sql ="UPDATE tickets
-    SET (customer_id, film_id) = ($1, $2)
+    SET (customer_id, screening_id) = ($1, $2)
     WHERE id = $3"
-    values = [@customer_id, @film_id, @id]
+    values = [@customer_id, @screening_id, @id]
     SqlRunner.run(sql,values)
   end
 
